4a334aa6757fd92b4f6c8806c96dc659358a9f36
Yawar Ahmad Khan <36065624+yawaRcan@users.noreply.github.com>
2024-05-20 17:43:51 +0500

    assets files

---
+++ resources/views/livewire/pages/auth/style.blade.php
@@ -0,0 +1,13 @@
+<link rel="shortcut icon" href="https://templates.iqonic.design/instadash/html/assets/images/favicon.ico" />
+      <link rel="stylesheet" href="../assets/css/backend-plugin.min28b5.css?v=2.0.0">
+      <link rel="stylesheet" href="../assets/css/backend28b5.css?v=2.0.0">
+      <link rel="stylesheet" href="../assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css">
+      <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+      <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
+      <link rel="stylesheet" href="../assets/vendor/%40icon/dripicons/dripicons.css">
+      
+      <link rel='stylesheet' href='../assets/vendor/fullcalendar/core/main.css' />
+      <link rel='stylesheet' href='../assets/vendor/fullcalendar/daygrid/main.css' />
+      <link rel='stylesheet' href='../assets/vendor/fullcalendar/timegrid/main.css' />
+      <link rel='stylesheet' href='../assets/vendor/fullcalendar/list/main.css' />
+      <link rel="stylesheet" href="../assets/vendor/mapbox/mapbox-gl.css">  </head>
