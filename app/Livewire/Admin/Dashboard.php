93029610e03aaac165b589da4b1840a7bd4acb64
Yawar Ahmad Khan <36065624+yawaRcan@users.noreply.github.com>
2024-05-20 17:41:17 +0500

    livewire components

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Livewire;
+namespace App\Livewire\Admin;
 
 use Livewire\Component;
 
@@ -8,6 +8,6 @@ class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard');
+        return view('livewire.admin.dashboard');
     }
 }
