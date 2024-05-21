df8e14dae71b067650c04158264253eb8f44e624
Yawar Ahmad Khan <36065624+yawaRcan@users.noreply.github.com>
2024-05-21 17:07:41 +0500

    pages completed

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -193,7 +193,11 @@
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
-                </a>
+                </a>                       
+
+                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                 @csrf
+                </form>
             </div>
         </li>
 
