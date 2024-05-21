a00d5621630d471f3e84899ea0a87911022b52e0
Yawar Ahmad Khan <36065624+yawaRcan@users.noreply.github.com>
2024-05-21 15:17:27 +0500

    admin component created in livewire

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
-
+    @livewireStyles
 
 
 </head>
@@ -131,5 +131,6 @@
     <!-- Page level custom scripts -->
     <script src="{{asset('assets/js/demo/chart-area-demo.js')}}"></script>
     <script src="{{asset('assets/js/demo/chart-pie-demo.js')}}"></script>
+    @livewireScripts
 </body>
 </html>
